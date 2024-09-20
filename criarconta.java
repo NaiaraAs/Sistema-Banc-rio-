980550b17596ee17a3e07f23b65ecde1afa15266
Naiara <natylynatiele@gmail.com>
2024-09-20 15:46:53 -0300

    Add files via upload

---
+++ criarconta.java
@@ -0,0 +1,26 @@
+public class criarconta {
+    
+     private String  titular,Agencia,DatadAbertura;
+     private int Nconta; 
+   private  double saldo;
+    
+    
+    
+        
+    
+       public contaBancaria(  titular, Nconta, Agencia,  saldo,  DatadAbertura ) {
+    
+            this.titular = titular;
+    
+            this.Nconta = Nconta;
+    
+            this.Agencia = Agencia;
+    
+            this.saldo = 0;
+    
+            this.DatadAbertura = DatadAbertura;				
+    
+        } //
+
+
+}// fim class
